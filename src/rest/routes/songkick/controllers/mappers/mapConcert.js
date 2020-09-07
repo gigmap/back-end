ab938d688d4816eeab917250291b5a4b873d61b9
treble-snake <treble.snake@gmail.com>
2020-09-07 21:20:37 +0300

    feat: add postponed and festival flags
    Read from Songkick this properties and send
    the flags to the front end.

--- src/rest/routes/songkick/controllers/mappers/mapConcert.js
+++ src/rest/routes/songkick/controllers/mappers/mapConcert.js
@@ -1,13 +1,18 @@
 const extractLocation = require('./extractLocation');
 const stripDateFromName = require('./stripDateFromName');
+const {
+  ATTENDANCE, CONCERT_STATUS, EVENT_TYPE
+} = require('../../../../../api/songkick/Constants');
 
 /**
  * @param {Concert} concert
+ * @param {string|undefined} attendance
  * @return {MappedConcert}
  */
-const mapConcert = concert => {
-  const {id, displayName, uri, start} = concert;
-  return {
+const mapConcert = (concert, attendance) => {
+  const {id, displayName, uri, start, status, type} = concert;
+  /** @type {MappedConcert} */
+  const result = {
     id,
     displayName: stripDateFromName(displayName),
     uri,
@@ -15,6 +20,22 @@ const mapConcert = concert => {
     start: start.date,
     members: []
   };
+
+  if (type === EVENT_TYPE.FESTIVAL) {
+    result.isFestival = true;
+  }
+
+  if (status === CONCERT_STATUS.POSTPONED) {
+    result.postponed = true;
+  }
+
+  if (attendance === ATTENDANCE.GOING) {
+    result.going = true;
+  } else if (attendance === ATTENDANCE.INTERESTED) {
+    result.interested = true;
+  }
+
+  return result;
 };
 
 module.exports = mapConcert;
